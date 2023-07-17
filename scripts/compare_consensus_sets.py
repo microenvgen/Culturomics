0c18f731360fd2b418abe919bf85f2a1596d3749
Alberto Rastrojo <alberto.rastrojo@uam.es>
2023-07-17 20:30:16 +0200

    Update compare_consensus_sets.py

--- scripts/compare_consensus_sets.py
+++ scripts/compare_consensus_sets.py
@@ -4,11 +4,25 @@
 ##########################################################################################
 """
 ------------------------------------------------------------------------------------------
-This script extract sequences from a genbank file and creates an 
-annotated fasta as follows:
+This scripts compares 2 consensus sequences sets from two different minion runs, and for
+each sequence in the first input file (reference) returns the best consensus sequences
+based on the Q-value between the 2 files (second file could contain more sequences, not
+present in first file, but only those sequences present in first file will be analysed). If
+a sequence is not present in second file, a warning will be shown and original sequence
+will be returned (from first file). IF both quailties values are equal first file consensus
+will be returned.
 
->seqID	fam	x1:y1:s1:product1:protid1 x2:y2:s2:product2:protid2....
-taggcggcttgactagcgcgatcagctagcgcgacgcgcgctatttcgagcatc....
+Both files must be rename as follows:
+
+>A7-A11 P5_P7_i5_i7_READS=10_VARIANTS=644_710_17_Q=0.007
+ATTGAACGCTGGCGGCAGGCCTAACACATGCAAGTCKAGCGGATGANNNNAGCTTGCTCNNGNATTNAG
+
+or,
+
+>A7-A11 READS=76_VARIANTS=0_2_0_Q=0.974
+AACGAACGCTGGCGGCAGGCTTAACACATGCAAGTCGAGCGCCCCGCAAG
+
+
 ------------------------------------------------------------------------------------------
 """
 ##########################################################################################
@@ -17,45 +31,70 @@ taggcggcttgactagcgcgatcagctagcgcgacgcgcgctatttcgagcatc....
 import sys
 import os
 import argparse
+from collections import defaultdict
 
 __author__ = "Alberto Rastrojo"
 __version_info__ = ('1','0','0')
 __version__ = '.'.join(__version_info__)
 
 def main():
-	
+
 	##########################################################################################
 	#--Argument
 	parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
-	parser.add_argument('input', type = str, help = '')
-	parser.add_argument('output', type = str, help = '.....')
+	parser.add_argument('first', type = str, help = 'First fasta file used as reference file')
+	parser.add_argument('second', type = str, help = 'Second fasta file to compare with')
+	parser.add_argument('output', type = str, help = 'Output fasta file name')
 	parser.add_argument('-x', dest = 'debug', action='store_true', default = False, help = 'Debug. Default: False')
 	parser.add_argument('-v', '--version', action='version', version=__file__ + ' v' + __version__)
 	args = parser.parse_args()
 	##########################################################################################
 
+	#--Reading second file
+	second = {name.split()[0]:(name, seq) for name, seq in fastaRead(args.second)}
 
+	#--Reading first file (reference) and writing best consensus
+	with open (args.output, 'w') as output:
+
+		for name, seq in fastaRead(args.first):
+			if second.get(name.split()[0]):
+				second_Q = float(second[name.split()[0]][0].split("_Q=")[1])
+				first_Q = float(name.split("_Q=")[1])
+				if second_Q > first_Q:
+					output.write(">{}\n{}\n".format(second[name.split()[0]][0], second[name.split()[0]][1]))
+				else:
+					output.write(">{}\n{}\n".format(name, seq))
+			else:
+				output.write(">{}\n{}\n".format(name, seq))
+
+def fastaRead(fasta, split_names=False):
+
+	with open(fasta, 'r') as infile:
+
+		seq = ''
+		for line in infile:
+
+			line = line.strip()
+
+			if line.startswith(">"):
+
+				if seq:
+
+					yield name, seq
+					seq = ''
+
+				name = line[1:]
+				if split_names: 
+					name = line.split()[0][1:]
+
+			else:
+				seq = seq + line
+
+	#--Last sequence
+	yield name, seq
 
 ##########################################################################################
 if __name__ == "__main__":
 
 	main()
 
-
-	# parser.add_argument('-t', dest = 'type', type = str, required = True, choices=['GTF', 'GFF'], help = 'file format: GTF or GFF. Default: GTF')
-	# parser.add_argument('-t', dest = 'table', type = str, required = True, help = 'tsv')
-	# parser.add_argument('files', nargs="*", type = str, help = '.....')
-	# parser.add_argument('-c', dest = 'cores', type = str, default = 4, help = 'number of threads')
-	# parser.add_argument('-l', '--min_aln_length', dest = 'minAlnLength', type = int, default = 100, help = 'Minimum alignment length')
-	
-	# #--Mutually exclusive options
-	# group = parser.add_mutually_exclusive_group(required=True)
-	# group.add_argument('-t', dest='taxo', type = str, help = 'Taxo from nuclid2taxo_v2.py')
-	# group.add_argument('-l', dest='labes', type = str, help = 'Label for all input sequences')
-
-	# parser.add_argument('-x', dest = 'debug', action='store_true', default = False, help = 'Debug. Default: False')
-
-
-	# args = parser.parse_args()
-
-	# #  outfile = os.path.basename(infile) + '.below'
