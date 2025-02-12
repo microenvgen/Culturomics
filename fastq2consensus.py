ceabd8c41d67c13b74afcb88b58f5612dd6102a3
Alberto Rastrojo <alberto.rastrojo@uam.es>
2025-02-12 12:32:43 +0100

    Update fastq2consensus.py

--- fastq2consensus.py
+++ fastq2consensus.py
@@ -164,7 +164,7 @@ def main():
 		#	with only 1 reads is assigned to 0 (q=0)
 
 
-		# q value,as is currently formulated es not very sensitive, however, using q2 = sqrt(reads)/(sqrt(reads)+variants)
+		# q value,as is currently formulated is not very sensitive, however, using q2 = sqrt(reads)/(sqrt(reads)+variants)
 		# the q2 value is more sensitive to the number of mutations when the number of reads is larger. 
 		variants = snvs + indels + deg
 		if reads == 1:
